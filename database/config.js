80193d603c3c6328c9067ba42d1465a0ec9049e5
Jhonni10 <jhonieripianoscue@gmail.com>
2023-08-28 14:01:37 -0500

    error al acceder a la base de datos

--- database/config.js
+++ database/config.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
-const dbConection = async()=>{
-
-   
+const dbConection = async () => {
     try {
-        await mongoose.connect(process.env.DB_CNN,{})
-         console.log('database online url:',process.env.DB_CNN);
-            
+        await mongoose.connect(process.env.DB_CNN , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database online. URL:', process.env.DB_CNN);
     } catch (error) {
-        throw new Error('Error a la hora de iniciar la base de datos ')
+        throw new Error('Error a la hora de iniciar la base de datos: ' + error.message);
     }
 }
 
 module.exports = {
     dbConection,
-
-}
+};
